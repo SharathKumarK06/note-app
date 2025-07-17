986110bcf7ba5c008121bf2e0852797bb0aee334
Sharath Kumar K <sharathkumar.k00@gmail.com>
2025-07-17 13:46:32 +0530

    Create functionality for list notes
    Create endpoints to list all notes/list note with id
    Create view associated with both endpoints

--- note/urls.py
+++ note/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # Read
+    # List all notes
+    path("", views.note_list, name="note_list"),
+    # Show one note
+    path("<int:note_id>/", views.note_item, name="note_item"),
 ]
--- note/views.py
+++ note/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
-def index(request):
-    return HttpResponse("Hello, World! You are at the index page")
+from .models import Note, Note_Tag, Tag
+from .forms import NoteForm
+
+# Read
+def note_list(request):
+    notes_list = Note.objects.all()
+    context = { "notes_list": notes_list }
+    return render(request, "note/index.html", context)
+
+# Read note with 'note_id'
+def note_item(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    tags_ids = list(Note_Tag.objects.filter(note_id=note_id)
+            .values_list('tag_id', flat=True))
+    tags = Tag.objects.filter(id__in=tags_ids)
+    context = { "note": note, "tags": tags }
+    return render(request, "note/note.html", context)
+
+
